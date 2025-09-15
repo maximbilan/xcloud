fbdb24da2cff4a936b0d72a3a8dd84caf4147fc7
Maksym Bilan <maximb.mail@gmail.com>
2025-09-15 12:35:17 +0300

    Deduplicate mocked test: keep only tests/http_mock_tests.rs
    - Remove duplicated http-mock test embedded in src/asc.rs
    - All tests pass under http-mock

--- src/asc.rs
+++ src/asc.rs
@@ -438,36 +438,4 @@ impl AppStoreConnectClient {
     }
 }
 
-#[cfg(all(test, feature = "http-mock"))]
-mod http_tests {
-    use super::*;
-    use httpmock::{Method::GET, MockServer};
-
-    #[tokio::test]
-    async fn list_ci_products_uses_custom_base_url() {
-        let server = MockServer::start();
-
-        let _m = server.mock(|when, then| {
-            when.method(GET)
-                .path("/v1/ciProducts")
-                .query_param("limit", "200");
-            then.status(200)
-                .header("content-type", "application/json")
-                .body("{\"data\":[{\"id\":\"prod1\",\"attributes\":{\"name\":\"Demo\"}}]}");
-        });
-
-        let cfg = Config {
-            issuer_id: "ignored".into(),
-            key_id: "ignored".into(),
-            p8_private_key_pem: "ignored".into(),
-        };
-        let client = AppStoreConnectClient::new(cfg, true)
-            .unwrap()
-            .with_static_token("test")
-            .with_base_url(Url::parse(&server.base_url()).unwrap());
-
-        let products = client.list_ci_products().await.unwrap();
-        assert_eq!(products.len(), 1);
-        assert_eq!(products[0]["id"], "prod1");
-    }
-}
+// mocked HTTP integration test lives in tests/http_mock_tests.rs
