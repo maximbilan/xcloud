0bf5caefc139dbeecbae2c9c39a998028ed7ab72
Maksym Bilan <maximb.mail@gmail.com>
2025-09-15 15:34:09 +0300

    feat: Add xcloud v0.1

--- Formula/xcloud.rb
+++ Formula/xcloud.rb
@@ -2,7 +2,7 @@ class Xcloud < Formula
   desc "A command line interface for xCloud"
   homepage "https://github.com/maximbilan/xcloud"
   url "https://github.com/maximbilan/xcloud/releases/download/v0.1/xcloud-0.1.tar.gz"
-  sha256 "b5b3ed9e42b7b6fd528e8fb65d22a2ede1ecab4dda0d2f47e0e07158fb12daa3"
+  sha256 "54d9d161c46d27a30eb393e4f8f224cd42bfe80d3d46011d2c75de5b8221adfe"
   version "0.1"
 
   def install
