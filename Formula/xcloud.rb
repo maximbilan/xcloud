414ceb617da85cb99185f8ab9a84a9436111a775
Maksym Bilan <maximb.mail@gmail.com>
2025-10-28 17:02:20 +0200

    Fix release URL - remove 'v' prefix from tag in download URL

--- Formula/xcloud.rb
+++ Formula/xcloud.rb
@@ -1,7 +1,7 @@
 class Xcloud < Formula
   desc "A command line interface for xCloud"
   homepage "https://github.com/maximbilan/xcloud"
-  url "https://github.com/maximbilan/xcloud/releases/download/v0.1/xcloud-0.1.tar.gz"
+  url "https://github.com/maximbilan/xcloud/releases/download/0.1/xcloud-0.1.tar.gz"
   sha256 "882cda045449873ce251514228fc7f91edb98e30ef2ba904c408e82752227c8f"
   version "0.1"
 
